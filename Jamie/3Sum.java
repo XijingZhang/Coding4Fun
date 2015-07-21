f533133dbc9cf68ffd656aca7d44267162cc5fa7
Jianmei Ye <yejm244@gmail.com>
2015-07-20 23:59:41 -0700

    3sum modified

--- Jamie/3Sum.java
+++ Jamie/3Sum.java
@@ -1,44 +1,50 @@
 //3Sum
-public ArrayList<ArrayList<Integer>> threeSum(int[] num){
-	ArrayList<ArrayList<Interger>> result = new ArrayList<ArrayList<Integer>>();
-	
-	if (num.length<3)
-		return result;
-	
-	//sort array
-	Arrays.sort(num);
-	
-	for(int i=0;i<num.length -2; i++){
-		if(i==0 ||num[i]>num[i-1]){
-			int negate = -num[i];
-			int start = i+1;
-			int end =num.length-1;
-			
-			while(start<end){
-				if(num[start]+num[end]==negate){
-					ArrayList<Integer> temp = new ArrayList<Integer>();
-					temp.add(num[i]);
-					temp.add(num[start]);
-					temp.add(num[end]);
-					
-					result.add(temp);
-					start++;
-					end--;
-					
-					//avoid duplicate solutions
-					while(start<end && num[end]==num[end+1])
-						end--;
-					while(start<end && num[start]==num[start-1])
-						start++;
-				}
-				else if(num[start]+num[end]<negate){
-					start++;
-				}else{
-					end--;
-				}
-			}
-		}
-	}
-	return result;
-	
-}
+public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+
+        if (nums.length < 3)
+            return result;
+
+        // sort array
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            //avoid duplicate solutions
+            if (i == 0 || nums[i] > nums[i - 1]) {
+
+                int negate = -nums[i];
+
+                int start = i + 1;
+                int end = nums.length - 1;
+
+                while (start < end) {
+                    //case 1
+                    if (nums[start] + nums[end] == negate) {
+                        List<Integer> temp = new ArrayList<Integer>();
+                        temp.add(nums[i]);
+                        temp.add(nums[start]);
+                        temp.add(nums[end]);
+
+                        result.add(temp);
+                        start++;
+                        end--;
+                        //avoid duplicate solutions
+                        while (start < end && nums[end] == nums[end + 1])
+                            end--;
+
+                        while (start < end && nums[start] == nums[start - 1])
+                            start++;
+                        //case 2
+                    } else if (nums[start] + nums[end] < negate) {
+                        start++;
+                        //case 3
+                    } else {
+                        end--;
+                    }
+                }
+
+            }
+        }
+
+        return result;
+    }
