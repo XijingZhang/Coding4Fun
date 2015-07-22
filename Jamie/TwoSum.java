42c630f9f25d938f9c5f17bfccb6886f3a16e5be
Jianmei Ye <yejm244@gmail.com>
2015-07-21 21:55:48 -0700

    Jamie's second commit

--- Jamie/TwoSum.java
+++ Jamie/TwoSum.java
@@ -2,15 +2,17 @@
 // Return indices of the two numbers such that they add up to the target, 
 // where index1 must be less than index2. 
 
-public int[] twoSum(int[] nums, int target){
+public int[] TwoSum(int[] nums, int target) {
 	// Creat an empty HashMap
 	Map<Integer, Integer> map = new HashMap<>();//"<>" valid in source 7
 	
-	for(int i=0; i < nums.length;i++){
+	for (int i = 0; i < nums.length; i++) {
 		int x = nums[i];
 		// If Hashtable didn't contains key of (target-x) at this time, then add it as new key:value, so that when it is being searched later, the returned index will be in ascending order
-		if(map.containsKey(target-x)){
-			return new int[]{map.get(target-x)+1,i+1};
+		if (map.containsKey(target - x)) {
+			return new int[]{
+				map.get(target - x) + 1, i + 1
+			};
 		}
 		map.put(x,i);
 		
