9d5a10662907af83e8981774c3f69a37fda84758
Jianmei Ye <yejm244@gmail.com>
2015-07-27 02:43:38 -0700

    Jamie's 6th commit

---
+++ Jamie/Searcha2DMatrix.java
@@ -0,0 +1,54 @@
+// Search a 2D Matrix 
+public boolean SearchMatrix1(int[][] matrix, int target) {
+	if (matrix == null || matrix.length == 0 || matrix[0].length ==0)
+		return false;
+	
+	int m = matrix.length;
+	int n = matrix[0].length;
+	int start = 0;
+	int end =m * n -1;
+	while (start < end) {
+		int mid = start + (end - start) / 2;
+		int midX = mid / n;
+		int midY = mid / m;
+		
+		if (matrix[midX][midY] == target)
+			return true;
+		if (matrix[midX][midY] < target) {
+			start = mid + 1;
+		} else {
+			end = mid - 1;
+		}
+	}
+	return false;
+}
+
+public boolean searchMatrix2(int[][] matrix, int target) {
+	if (matrix == null || matrix.length == 0) {
+		return false;
+	}
+	if (matrix[0] == null || matrix[0].length == 0) {
+		return false;
+	}
+	
+	int row = matrix.length, column = matrix[0].length;
+	int start = 0, end = row * column - 1;
+	
+	while(start + 1 < end) {
+		int mid = start + (end - start) / 2;
+		int number = matrix[mid / column][mid % columns];
+		if (number == target) {
+			return true;
+		} else if (number < target) {
+			start = mid;
+		} else {
+			end = mid;
+		}
+	}
+	if (matrix[start / column][start % column] == target) {
+		return true;
+	} else if (matrix[end / column][end % column] == target) {
+		return true;
+	}
+	return false;
+}
