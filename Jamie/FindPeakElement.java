4578637f64e19a112e229c7d3e785b8bcf41dc74
Jianmei Ye <yejm244@gmail.com>
2015-08-01 17:28:17 -0700

    Jamie's 10th commit.

--- Jamie/FindPeakElement.java
+++ Jamie/FindPeakElement.java
@@ -1,4 +1,5 @@
 //Find Peak Element
+// O(n)
 public int FindPeakElement(int[] nums) {
 	int max = nums[0];
 	int index = 0;
@@ -18,3 +19,23 @@ public int FindPeakElement(int[] nums) {
 	return index;
 }
 
+// Optimal soluiton : O(lgn)
+
+public int findPeakElement(int[] nums) {
+     int start = 0, end = nums.length  - 1; 
+     while(start + 1 <  end) {
+		 int mid = (start + end) / 2;
+         if(nums[mid] < nums[mid - 1]) {
+			 end = mid;
+         } else if(nums[mid] < nums[mid + 1]) {
+			 start = mid;
+         } else {
+             end = mid;
+         }
+    }
+    if(nums[start] < nums[end]) {
+		return end;
+    } else { 
+        return start;
+    }
+}
